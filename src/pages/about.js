d41b1ff56a6a9e6a327c41ab89d0965d48e01066
Kanami <kanami.h217@gmail.com>
2021-09-02 13:49:29 -0400

    updated contents

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,34 +2,50 @@ import React from "react"
 import Layout from "../components/Layout"
 import { Link } from "gatsby"
 import * as styles from "../styles/about.module.css"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function About() {
   return (
     <Layout>
-      <section>
-        <h2>Hi! I'm Kanami</h2>
-        <h3>A Well-Oriented Great Potential Front-End Developer</h3>
-        <p>
-          Highly motivated individual currently seeking a front-end engineering
-          position. A passion to find better solutions, developing and
-          simplifying procedures, and having a strong curiosity to learn new
-          technologies. Skilled communicator with a sales background.
-        </p>
+      <section className={styles.aboutWrapper}>
+        <div className={styles.introduction}>
+          <h2>Hi! I'm Kanami</h2>
+          <p>
+            I'm a <span className={styles.bold}>web developer</span> &
+            <span className={styles.bold}> designer</span> who loves finding
+            solutions with technology. I have worked for small businesses as a
+            freelance web developer in Japan. After that, I enrolled in
+            Interactive Media Design at{" "}
+            <Link to="https://www.senecacollege.ca/programs/fulltime/INM.html">
+              Seneca College
+            </Link>{" "}
+            to earn better design and development knowledge. I'm passionate
+            about learning new web technologies and enjoy working on UX case
+            studies, especially user research and prototyping.
+          </p>
+        </div>
+        <div className={styles.aboutImg}>
+          <StaticImage
+            src="../images/flamingo_laptop.png"
+            alt="A flamingo typing something on laptop"
+          />
+        </div>
         <div className={styles.experience}>
           <h2 className={styles.title}>Experiences</h2>
           <ul>
             <li>
-              3 years of freelance front-end development experience with
-              HTML/CSS, JavaScript, React, Bootstrap and WordPress.
+              3 years of freelance front-end development experience with HTML,
+              CSS, Bootstrap, WordPress, JavaScript, React and Gatsby.
             </li>
             <li>
               Directly contacted clients, scooped up their needs, and proposed a
               best solution to solve their problems.
             </li>
           </ul>
-          <h3>Other Skills</h3>
+        </div>
+        <div>
+          <h2>Other Skills</h2>
           <ul>
-            <li>Fluent in English</li>
             <li>Native Japanese speaker</li>
             <li>Have experience in teaching HTML/CSS</li>
             <li>Have experience as a volunteer Japanese language tutor</li>
@@ -38,10 +54,10 @@ export default function About() {
         <div className={styles.achievement}>
           <h2>Certificate</h2>
           <div>
-            <h3>
+            <p>
               edX Verified Certificate for Computer Science 101 Stanford
               University
-            </h3>
+            </p>
             <Link to="https://courses.edx.org/certificates/59f3fa8e9cea40e2be9e05fd00bd474d">
               See credential
             </Link>
