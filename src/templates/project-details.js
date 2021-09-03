a87f4989826bd924691d8158e817a92ef7739cf0
Kanami <kanami.h217@gmail.com>
2021-09-03 08:00:08 -0400

    fixed iproblems that couldn't show image on project detail page

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -6,17 +6,16 @@ import * as styles from "../styles/project-details.module.css"
 
 export default function projectDetails({ data }) {
   const { html } = data.markdownRemark
-  const { title, stack, detailedImg } = data.markdownRemark.frontmatter
-  console.log(data.markdownRemark.frontmatter)
-
+  const { title, stack, featured } = data.markdownRemark.frontmatter
+  console.log(data.markdownRemark.frontmatter.featured)
   return (
     <Layout>
       <div className={styles.details}>
         <h2>{title}</h2>
         <h3>{stack}</h3>
-        <div className={styles.featured}>
-          <Img fluid={detailedImg.childImageSharp.fluid} />
-        </div>
+        {/*<div className={styles.featured}>
+          <Img fluid={featured} />
+  </div>:*/}
         {
           <div
             className={styles.html}
@@ -33,15 +32,7 @@ export const query = graphql`
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
-        stack
-        title
-        detailedImg {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
+        featured
       }
     }
   }
